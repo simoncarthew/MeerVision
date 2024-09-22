55c732175a8238e5e1c61d0c4cc9beb7e3d7083a
simoncarthew <carthewsimone@gmail.com>
2024-09-22 13:52:44 +0200

    Overide button functions

--- Control/Control.py
+++ Control/Control.py
@@ -17,12 +17,32 @@ class Control:
         self.lcd = LCD()
         self.rtc = RTC()
         self.buttons = Buttons(pin_ok=23, pin_up=27, pin_down=22, pin_back=24)
+        
+        # Override button actions
+        self.buttons.ok_action = self.ok_action
+        self.buttons.back_action = self.back_action
+        self.buttons.up_action = self.up_action
+        self.buttons.down_action = self.down_action
 
+    def ok_action(self):
+        print("Control: OK action")
+        # Add your custom OK action here
+
+    def back_action(self):
+        print("Control: Back action")
+        # Add your custom Back action here
+
+    def up_action(self):
+        print("Control: Up action")
+        # Add your custom Up action here
+
+    def down_action(self):
+        print("Control: Down action")
 if __name__ == "__main__":
     control = Control()
     try:
         while True:
-            print(control.buttons.get_button_states())
+            # print(control.buttons.get_button_states())
             sleep(0.1)  # Add a small delay to prevent CPU overuse
     except KeyboardInterrupt:
         print("Program terminated by user")
