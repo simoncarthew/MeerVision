03153173069d7b1dc62fbd8b30a0ac233071d776
simoncarthew <carthewsimone@gmail.com>
2024-09-30 18:54:51 +0200

    Testing yolo5 on pi start

--- Control/Process.py
+++ Control/Process.py
@@ -1,3 +1,17 @@
-from ObjectDetection.Yolo5 import yolo5
-from ObjectDetection.Yolo8 import yolo8
-from ObjectDetection.Megadetector import Mega
+# standard imports
+import sys
+import os
+
+# add relatove folders to system path
+sys.path.append(os.path.join("ObjectDetection","Yolo5"))
+sys.path.append(os.path.join("ObjectDetection","Yolo8"))
+sys.path.append(os.path.join("ObjectDetection","Megadetector"))
+
+# import assistive classes
+from yolo5 import Yolo5
+from yolo8 import Yolo8
+from Mega import Mega
+
+class Process:
+    def __init__(self):
+        self.yolo5 = Yolo5(model_size='s', device='cpu')
